66542143218322f035bc0ed44535fa56bcb30990
Sowrub mojumder <nazmulhasun7@gmail.com>
2023-05-30 08:11:16 -0700

    delete cart

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -28,11 +28,40 @@ async function run() {
         // await client.connect();
 
         const menuCollection = client.db("bistroDB").collection("menu");
+        const reviewCollection = client.db("bistroDB").collection("reviews");
+        const cartCollection = client.db("bistroDB").collection("carts");
         // bistroDB.menu
 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result);
+        })
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
+        app.get('/carts', async(req, res) => {
+            const email =req.query.email;
+            // console.log(email);
+            if(!email){
+                res.send([])
+            }
+            const query ={email: email};
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.post('/carts',  async(req, res)=>{
+            const item =req.body;
+            console.log(item);
+            const result= await cartCollection.insertOne(item)
+            res.send(result)
+        })
+
+        app.delete('/carts/:id', async(req, res)=>{
+            const id =req.params.id
+            const query ={_id:new ObjectId(id)}
+            const result =await cartCollection.deleteOne(query)
+            res.send(result)
         })
 
         // Send a ping to confirm a successful connection
